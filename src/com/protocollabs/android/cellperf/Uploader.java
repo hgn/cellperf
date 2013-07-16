b4a99e8980a093651f375bc674e60b627be2f8ac
Hagen Paul Pfeifer <hagen@jauu.net>
2013-07-16 22:29:31 +0200

    make uploader buildable
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- src/com/protocollabs/android/cellperf/Uploader.java
+++ src/com/protocollabs/android/cellperf/Uploader.java
@@ -1,35 +1,95 @@
 package com.protocollabs.android.cellperf;
 
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.net.URI;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 import android.os.Bundle;
 
 
-public class Pinger {
+public class Uploader {
 
-    public static String ping(String url) {
+    private static final String url = "http://foo.appspot.com";
 
-        URI website = new URI("http://foo.appspot.com");
+
+    public static void ping(String url) {
+
+        URI website;
+
+        try {
+            website = new URI(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
         HttpClient client = new DefaultHttpClient();
         HttpPost request = new HttpPost(website);
 
         request.setHeader("Content-Type", "application/json");
+        request.setHeader("Accept", "application/json");
 
-        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        String currentDateTimeString = new SimpleDateFormat("HH:mm:ss").format(new Date());
 
         JSONObject obj = new JSONObject();
-        obj.put("reg_id","Registration ID sent to the server"); 
-        obj.put("datetime",currentDateTimeString);
+        try {
+            obj.put("reg_id","Registration ID sent to the server"); 
+            obj.put("datetime",currentDateTimeString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
 
-        StringEntity se = new StringEntity(obj.toString());
+        StringEntity se;
+        try {
+            se = new StringEntity(obj.toString());
+        } catch (java.io.UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return;
+        }
+
         request.setEntity(se);
-        HttpResponse response = client.execute(request);
+        HttpResponse response;
+        try {
+            response = client.execute(request);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
-        String out = EntityUtils.toString(response.getEntity());
+        try {
+            String out = EntityUtils.toString(response.getEntity());
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        return;
     }
+
 }
