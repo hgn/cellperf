309921122d9fc8bde0cf6c39add44be3f079698e
Hagen Paul Pfeifer <hagen@jauu.net>
2013-07-25 22:44:17 +0200

    update cellperf logo
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- res/raw/rebuild.sh
+++ res/raw/rebuild.sh
@@ -2,8 +2,8 @@
 
 for SVG in *.svg
 do PNG=`basename $SVG svg`png
+	inkscape -z  -e ../drawable-xxhdpi/$PNG -w 128 -h 128 $SVG
 	inkscape -z  -e ../drawable-xhdpi/$PNG -w 96 -h 96 $SVG
 	inkscape -z  -e ../drawable-hdpi/$PNG -w 72 -h 72 $SVG
 	inkscape -z  -e ../drawable-mdpi/$PNG -w 48 -h 48 $SVG
-	inkscape -z  -e ../drawable-ldpi/$PNG -w 36 -h 36 $SVG
 done
